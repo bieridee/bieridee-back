21218805880a5c62aa0981829ae8180be2778564
Jonas Furrer <jfurrer@hsr.ch>
2012-04-02 19:49:41 +0200

    Updated BeerListResource to use the new Utils

--- src/main/java/ch/hsr/bieridee/resourcehandler/BeerListResource.java
+++ src/main/java/ch/hsr/bieridee/resourcehandler/BeerListResource.java
@@ -1,6 +1,5 @@
 package ch.hsr.bieridee.resourcehandler;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import org.neo4j.graphdb.Node;
@@ -8,24 +7,24 @@ import org.restlet.resource.ServerResource;
 
 import ch.hsr.bieridee.domain.Beer;
 import ch.hsr.bieridee.models.BeerModel;
-import ch.hsr.bieridee.resourcehandler.interfaces.IBierListResource;
+import ch.hsr.bieridee.resourcehandler.interfaces.IBeerListResource;
 import ch.hsr.bieridee.utils.DBUtil;
+import ch.hsr.bieridee.utils.DomainConverter;
 
 /**
- * @author chrigi ServerResource for getting a List of Beers.
+ * ServerResource for getting a List of Beers.
+ * 
+ * @author cfaessle, jfurrer 
  */
-public class BeerListResource extends ServerResource implements IBierListResource {
+public class BeerListResource extends ServerResource implements IBeerListResource {
 
 	@Override
 	public List<Beer> retrieve() {
 
-		final List<Beer> beers = new LinkedList<Beer>();
 		final List<Node> beerNodes = DBUtil.getBeerNodeList();
-		for (Node beerNode : beerNodes) {
-			final BeerModel bm = new BeerModel(beerNode.getId());
-			beers.add(bm.getDomainObject());
-		}
-
+		final List<BeerModel> beerModels= DomainConverter.createBeerModelsFromList(beerNodes);
+		final List<Beer> beers = DomainConverter.extractDomainObjectFromModel(beerModels);
+		
 		return beers;
 	}
 
