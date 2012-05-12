7f01033ebe42d6489ddce37d4096411eed82772e
Chrigi Faessler <cfaessle@hsr.ch>
2012-05-12 16:06:53 +0200

    Userfilter in timeline eingefuegt

--- src/main/java/ch/hsr/bieridee/resourcehandler/TimelineResource.java
+++ src/main/java/ch/hsr/bieridee/resourcehandler/TimelineResource.java
@@ -2,7 +2,7 @@ package ch.hsr.bieridee.resourcehandler;
 
 import java.util.List;
 
-import org.neo4j.server.rest.web.NodeNotFoundException;
+import org.neo4j.graphdb.NotFoundException;
 import org.restlet.ext.jackson.JacksonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.ServerResource;
@@ -12,31 +12,31 @@ import ch.hsr.bieridee.config.Res;
 import ch.hsr.bieridee.exceptions.WrongNodeTypeException;
 import ch.hsr.bieridee.models.AbstractActionModel;
 import ch.hsr.bieridee.models.TimelineModel;
+import ch.hsr.bieridee.models.UserModel;
 import ch.hsr.bieridee.resourcehandler.interfaces.IReadOnlyResource;
 
 /**
  * Timeline resource.
- *
+ * 
  */
 public class TimelineResource extends ServerResource implements IReadOnlyResource {
 
 	@Override
-	public Representation retrieve() throws WrongNodeTypeException, NodeNotFoundException {
-		String username;
-		final List<AbstractActionModel> actionModels;
+	public Representation retrieve() throws WrongNodeTypeException, NotFoundException {
 		final String usernameParam = getQuery().getFirstValue(Res.TIMELINE_FILTER_PARAMETER_USER);
-		if(usernameParam == null){
+		final List<AbstractActionModel> actionModels;
+		if (usernameParam == null) {
 			actionModels = TimelineModel.getAll();
-		}
-		else{
+		} else if (!UserModel.doesUserExist(usernameParam)) { // Invalid Username
+			throw new NotFoundException("Username invalid");
+		} else {
 			actionModels = TimelineModel.getAllForUser(usernameParam);
 		}
-		
+
 		final AbstractActionModel[] actionModelArray = actionModels.toArray(new AbstractActionModel[actionModels.size()]);
 		final JacksonRepresentation<AbstractActionModel[]> actionsRep = new JacksonRepresentation<AbstractActionModel[]>(actionModelArray);
 		actionsRep.setObjectMapper(Config.getObjectMapper());
-		
+
 		return actionsRep;
 	}
-
 }
