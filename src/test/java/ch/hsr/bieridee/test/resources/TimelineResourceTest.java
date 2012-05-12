0d49dcde4aa27202133812028454af8fefc20bff
Chrigi Faessler <cfaessle@hsr.ch>
2012-05-12 16:27:45 +0200

    Test fuer Filterfunktion hinzugefuegt

--- src/test/java/ch/hsr/bieridee/test/resources/TimelineResourceTest.java
+++ src/test/java/ch/hsr/bieridee/test/resources/TimelineResourceTest.java
@@ -11,16 +11,16 @@ import ch.hsr.bieridee.config.Res;
 
 /**
  * Tests the timeline resource.
- *
+ * 
  */
 public class TimelineResourceTest extends ResourceTest {
-	
+
 	/**
 	 * Tests the retrieval of the timeline.
 	 */
 	@Test
 	public void retrieveTimeline() {
-		
+
 		final JSONArray timelineJson = getJSONArray(Res.PUBLIC_API_URL + Res.TIMELINE_COLLECTION);
 		JSONObject firstEntry = null;
 		JSONObject thirdEntry = null;
@@ -31,7 +31,7 @@ public class TimelineResourceTest extends ResourceTest {
 			Assert.fail();
 			e.printStackTrace();
 		}
-		
+
 		Assert.assertNotNull(firstEntry);
 		Assert.assertNotNull(thirdEntry);
 		try {
@@ -45,7 +45,25 @@ public class TimelineResourceTest extends ResourceTest {
 			Assert.fail();
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
+	@Test
+	public void retrieveFilteredTimeline() {
+		final String username = "alki";
+		final String request = Res.PUBLIC_API_URL + Res.TIMELINE_COLLECTION + "?username=" + username;
+		final JSONArray timelineJson = getJSONArray(request);
+		System.out.println("request: " + request + "\tjson: " + timelineJson);
+
+		for (int i = 0; i < timelineJson.length(); ++i) {
+			try {
+				final JSONObject jsonObject = (JSONObject) timelineJson.get(i);
+				final JSONObject user = jsonObject.getJSONObject("user");
+				Assert.assertEquals(username, user.get("user"));
+			} catch (JSONException e) {
+				Assert.fail();
+				e.printStackTrace();
+			}
+		}
+	}
 }
