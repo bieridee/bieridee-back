57613d10029ac88783109d173b071ac255088bdc
Chrigi Faessler <cfaessle@hsr.ch>
2012-05-18 17:58:41 +0200

    Exception angepasst

--- src/main/java/ch/hsr/bieridee/resourcehandler/BrewerySizeResource.java
+++ src/main/java/ch/hsr/bieridee/resourcehandler/BrewerySizeResource.java
@@ -1,12 +1,12 @@
 package ch.hsr.bieridee.resourcehandler;
 
-import org.neo4j.server.rest.web.NodeNotFoundException;
 import org.restlet.ext.jackson.JacksonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.ServerResource;
 
 import ch.hsr.bieridee.config.Config;
 import ch.hsr.bieridee.domain.BrewerySize;
+import org.neo4j.graphdb.NotFoundException;
 import ch.hsr.bieridee.exceptions.WrongNodeTypeException;
 import ch.hsr.bieridee.resourcehandler.interfaces.IReadOnlyResource;
 
@@ -16,7 +16,7 @@ import ch.hsr.bieridee.resourcehandler.interfaces.IReadOnlyResource;
  */
 public class BrewerySizeResource extends ServerResource implements IReadOnlyResource {
 	@Override
-	public Representation retrieve() throws WrongNodeTypeException, NodeNotFoundException {
+	public Representation retrieve() throws WrongNodeTypeException, NotFoundException {
 		final JacksonRepresentation<String[]> jacksonRep = new JacksonRepresentation<String[]>(BrewerySize.SIZES);
 		jacksonRep.setObjectMapper(Config.getObjectMapper());
 		return jacksonRep;
